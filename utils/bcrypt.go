54046b43043d48c493b8d3059ca6342421f0ba27
lipaysam <lipaysamart@gmail.com>
2025-04-18 18:28:48 -0700

    fix

---
+++ utils/bcrypt.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashAndSalt(pass []byte) string {
+	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+	if err != nil {
+		log.Printf("Failed to generate password: %v", err)
+		return ""
+	}
+	return string(hashed)
+}
